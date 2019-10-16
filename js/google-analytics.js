86fbbae812afdf53111fcf86a282edb8b33f1228
0And1Story <witherman233@gmail.com>
2019-10-16 22:48:15 +0800

    Add "Google Analytics"

---
+++ js/google-analytics.js
@@ -0,0 +1,7 @@
+(function() {
+    document.write('<script src="https://www.googletagmanager.com/gtag/js?id=UA-150196424-1" async></script>');
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-150196424-1');
+})();
